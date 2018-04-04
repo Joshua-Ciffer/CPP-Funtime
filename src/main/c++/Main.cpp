73595ecaed0b697a702f234de3f8e432db5fbd3b
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-04-03 20:10:20 -0400

    Created main entry point.

--- src/main/c++/Main.cpp
+++ src/main/c++/Main.cpp
@@ -1,12 +1,11 @@
 // Author: Joshua Ciffer
-// Version: 04/02/2018
+// Version: 04/03/2018
 
 #include <iostream>
-#include <HelloWorld.cpp>
 
 using namespace std;
 
 int main() {
-	helloWorld();
+
 	return 0;
 }
