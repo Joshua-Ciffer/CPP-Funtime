26575cf4320a49e2116bc2097d320869551d1add
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-04-19 02:54:06 -0400

    Played around with java IO Streams.

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+@SuppressWarnings("javadoc")
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		String str = "Hello World!";
+		File outputFile = new File("C:\\Users\\Joshua\\Desktop\\File.bin");
+		FileOutputStream outputStream = new FileOutputStream(outputFile);
+		outputStream.write(str.getBytes());
+		outputStream.close();
+	}
+	
+}
