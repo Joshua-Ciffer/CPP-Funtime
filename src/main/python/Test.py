7994802a4105b7a0dbd5feddedf4b0c95e20d5a1
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-05-24 08:09:25 -0400

    Did some python shit.

--- src/main/python/Test.py
+++ src/main/python/Test.py
@@ -1,9 +1,10 @@
 # Author: Joshua Ciffer
 # Date: 03/19/2018
 def main():
-    print("Hello World!")
-    x = input("Enter some stuff: ")
-    print(x)
-    
-
+	print("Enter a word faggot: ")
+	word = input()
+	print("How many times should the word be repeated?: ")
+	num = int(input())
+	print(word * num, "is", str(len(word * num)), "characters long.")
+	
 main()
